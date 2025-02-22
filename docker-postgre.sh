2bed35e233818b33a5291ea5d014ab8f20ca8845
İlham Ahmadzada <ahmedzada.ilham@gmail.com>
2025-02-23 00:13:58 +0400

    add port

--- docker-postgre.sh
+++ docker-postgre.sh
@@ -2,5 +2,6 @@ $ docker run -d \
 	--name boe-db \
 	-e POSTGRES_PASSWORD=postgres \
 	-e PGDATA=/var/lib/postgresql/data/pgdata \
+    -p 5432:5432 \
 	-v /home/ilham/project/data:/var/lib/postgresql/data \
 	postgres
