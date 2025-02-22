5ad273497b139d5cf8ce8c523da4b758c735f51f
İlham Ahmadzada <ahmedzada.ilham@gmail.com>
2025-02-23 00:10:31 +0400

    add docker-postgre

---
+++ docker-postgre.sh
@@ -0,0 +1,6 @@
+$ docker run -d \
+	--name some-postgres \
+	-e POSTGRES_PASSWORD=mysecretpassword \
+	-e PGDATA=/var/lib/postgresql/data/pgdata \
+	-v /home/ilham/project/data:/var/lib/postgresql/data \
+	postgres
